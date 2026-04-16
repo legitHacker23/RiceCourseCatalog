1cb4e79b88bf5bb9b6c66840bbf24c12a787561e
legitHacker23 <nbmendoza1432@gmail.com>
2026-04-16 15:33:15 -0500

    url change

--- backend/app.py
+++ backend/app.py
@@ -53,7 +53,7 @@ CORS(app, origins=[
     'https://main.d138s9ngzid9pl.amplifyapp.com', # Amplify frontend
     'https://www.ricecatalog.org',  # Production frontend
     'https://ricecatalog.org',
-    'https://rice-course-catalog-ktn1.vercel.app',  # Production frontend (without www)
+    'https://rice-course-catalog-ktn1-p9kkm7en0-noah-mendozas-projects.vercel.app/',  # Production frontend (without www)
 ], 
      allow_headers=['Content-Type', 'Authorization'],
      methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'])
